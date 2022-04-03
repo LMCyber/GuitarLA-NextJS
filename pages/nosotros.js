8722c5ad7cef53c0c39fb1fe1e2ede627f24da14
Luis <lmendozag.lmmg@gmail.com>
2022-04-02 21:07:28 -0600

    Clase 204

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,9 +1,43 @@
+import Image from 'next/image'
 import { Layout } from '../components/Layout'
+import styles from '../styles/Nosotros.module.css'
 
 const Nosotros = () => {
   return (
     <Layout pagina='Nosotros'>
-      <h1>Desde Nosotros </h1>
+      <main className='contenedor'>
+        <h2 className='heading'>Nosotros</h2>
+
+        <div className={styles.contenido}>
+          <Image
+            layout='responsive'
+            width={600}
+            height={450}
+            src='/img/nosotros.jpg'
+            alt='Imagen sobre nosotros'
+          />
+
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+              nesciunt laudantium odit aut neque quos enim ab, quas recusandae
+              ipsum praesentium tempora hic soluta exercitationem commodi
+              eligendi eius placeat dolorem! Libero eum fugiat consequuntur quae
+              dicta optio quisquam, voluptatum officia voluptatibus non
+              deleniti, facilis quidem error laudantium ratione voluptatem x
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+              nesciunt laudantium odit aut neque quos enim ab, quas recusandae
+              ipsum praesentium tempora hic soluta exercitationem commodi
+              eligendi eius placeat dolorem! Libero eum fugiat consequuntur quae
+              dicta optio quisquam, voluptatum officia voluptatibus non
+              deleniti, facilis quidem error laudantium ratione voluptatem x
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
